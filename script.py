eba8a9a90b5e156413dbf249752ebe43be565cbb
vihaannnn <vihaan.nama@outlook.com>
2024-08-24 12:35:35 -0400

    Started the README.md file, created a basic graph of Total Living Cost Index vs. Total Food Cost Index, and added a requirements.txt file

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+
 
 df1 = pd.read_csv("Cost_of_Living_Index_by_Country_2024.csv")
 
@@ -11,3 +13,12 @@ df1['Total Food Cost Index'] = df1.apply(lambda x: x['Groceries Index'] + x['Res
 df1.rename(columns = {"Cost of Living Plus Rent Index" : "Total Living Cost Index"}, inplace = True, errors = 'raise')
 print(df1)
 
+# x =[5, 7, 8, 7, 2, 17, 2, 9,
+#     4, 11, 12, 9, 6] 
+ 
+# y =[99, 86, 87, 88, 100, 86, 
+#     103, 87, 94, 78, 77, 85, 86]
+ 
+plt.scatter(df1['Total Living Cost Index'], df1['Total Food Cost Index'], c ="blue")
+
+plt.show()
