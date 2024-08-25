4198821a2e84ff9fe743e3d94547d50bc7e127ee
vihaannnn <vihaan.nama@outlook.com>
2024-08-25 14:37:00 -0400

    Cleaned up code and added Analysis along with requirements

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # 510PreReq
 This Repository is for the Pre-Requisite for the AIPI course 510, created by Vihaan Nama.
-source venv/bin/activate
-
 
 ```markdown
 # Living Cost, Food Cost and Purchasing Power Trend Analysis
--- workingDir/script.py
+++ workingDir/script.py
@@ -10,9 +10,7 @@ def dataProcessor(df1):
 
     print('\n\n\n')
 
-    # df1['Total Food Cost'] = df1['Groceries Index'] + df1['Restaurant Price Index']
     df1['Total Food Cost Index'] = df1.apply(lambda x: x['Groceries Index'] + x['Restaurant Price Index'], axis=1)
-    # print df
 
     df1.rename(columns = {"Cost of Living Plus Rent Index" : "Total Living Cost Index"}, inplace = True, errors = 'raise')
 
@@ -44,8 +42,6 @@ def createScatterPlot(df1, name):
 
     txt="This graph describes 3 different values, IN RED - Local Purchasing Power Index (x-axis) vs. Total Food Cost Index (y-axis), IN GREEN - Local Purchasing Power Index (x-axis) vs. Total Living Cost Index (y-axis), and IN BLUE - Total Living Cost Index (x-axis) vs. Total Food Cost Index (y-axis)"
     plt.figtext(0.5, 0.01, txt, wrap=True, horizontalalignment='center', fontsize=12)
-
-    # plt.show()
     
     plt.savefig(name)
 
@@ -60,6 +56,14 @@ def main():
     df1 = dataProcessor(df1)
 
     createScatterPlot(df1, 'Cost_Index_Analysis.png')
+
+    print("Please find the graph generated in the 'workingDir' saved as 'Cost_Index_Analysis.png'")
+
+    print('\n\n\n')
+    print("GRAPH ANALYSIS")
+    print("Upon Analyzing this graph we notice - with even a small change in the food cost index, the living cost increases linearly, but with a higher slope that then other dependencies (seen from the points in blue)")
+    print("If we look at the points in red and green, when the Local Purchasing Power Index increases,the Total Living Cost Index increases lesser compared to the Total Food Cost Index.")
+    print("We notice that the red and green points are more scattered compared to the blue ones, which indicates that food and living cost indexes are highly correlated compared to the indvidual indexes and the purchasing power index")
 
 if __name__ == "__main__":
     main()
